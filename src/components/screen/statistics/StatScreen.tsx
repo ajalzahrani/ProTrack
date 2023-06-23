da047364ea20fc7dbaff5def5dbab0129cd72f7f
Abdulrhaman Alzahrani <alzahrani.abdulrahman3@gmail.com>
2023-06-23 11:44:06 +0300

    Adjusting custom tab-view

--- src/components/screen/statistics/StatScreen.tsx
+++ src/components/screen/statistics/StatScreen.tsx
@@ -1,50 +1,90 @@
 import React, {useState} from 'react';
-import {useWindowDimensions} from 'react-native';
+import {
+  Animated,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  StatusBar,
+} from 'react-native';
 import {TabView, SceneMap} from 'react-native-tab-view';
-import {colors} from 'src/assets/theme';
+import {ScreenContainer} from 'src/components/shared';
 import ProgressView from './components/ProgressView';
 import HistoryView from './components/HistoryView';
 import StatisticView from './components/StatisticView';
-import ScreenContainer from 'src/components/shared/ScreenContainer';
 
-const HistoryRoute = () => {
-  return <HistoryView />;
-};
-const StatisticRoute = () => {
-  return <StatisticView />;
-};
-const ProgressRoute = () => {
-  return <ProgressView />;
-};
-const renderScene = SceneMap({
-  history: HistoryRoute,
-  progress: ProgressRoute,
-  statistic: StatisticRoute,
-});
-
-const StatScreen = () => {
-  const layout = useWindowDimensions();
-
+const TabViewExample = () => {
   const [index, setIndex] = useState(0);
-  const [routes] = useState([
-    {key: 'first', title: 'History'},
-    {key: 'second', title: 'Progress'},
-    {key: 'third', title: 'Statistic'},
-  ]);
+
+  const routes = [
+    {key: 'history', title: 'History'},
+    {key: 'progress', title: 'Progress'},
+    {key: 'statistic', title: 'Statistic'},
+  ];
+
+  const handleIndexChange = index => {
+    setIndex(index);
+  };
+
+  const renderTabBar = props => {
+    const inputRange = props.navigationState.routes.map((x, i) => i);
+    return (
+      <View style={styles.tabBar}>
+        {props.navigationState.routes.map((route, i) => {
+          const opacity = props.position.interpolate({
+            inputRange,
+            outputRange: inputRange.map((inputIndex: any) =>
+              inputIndex === i ? 1 : 0.5,
+            ),
+          });
+
+          return (
+            <TouchableOpacity
+              style={styles.tabItem}
+              onPress={() => setIndex(i)}
+              key={i}>
+              <Animated.Text style={{opacity}}>{route.title}</Animated.Text>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    );
+  };
+
+  const renderScene = SceneMap({
+    history: HistoryView,
+    progress: ProgressView,
+    statistic: StatisticView,
+  });
 
   return (
     <ScreenContainer>
       <TabView
         navigationState={{index, routes}}
-        onIndexChange={setIndex}
-        renderScene={SceneMap({
-          first: HistoryView,
-          second: ProgressView,
-          third: StatisticView,
-        })}
+        renderScene={renderScene}
+        renderTabBar={renderTabBar}
+        onIndexChange={handleIndexChange}
       />
     </ScreenContainer>
   );
 };
 
-export default StatScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  tabBar: {
+    flexDirection: 'row',
+    // paddingTop: StatusBar.currentHeight,
+    backgroundColor: 'whtie',
+    marginHorizontal: 20,
+  },
+  tabItem: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 16,
+    marginHorizontal: 2,
+    backgroundColor: 'white',
+  },
+});
+
+export default TabViewExample;
